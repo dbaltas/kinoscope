875936733bb38e58e393dd893f39c6b5b67ae8ae
dbaltas <dbaltas@672ca433-b4e7-4185-b187-f88d0d116b9b>
2012-03-12 00:10:25 +0000

    refs #12: exporting a run into csv
    git-svn-id: https://codemax.unfuddle.com/svn/codemax_observador-svn@55 672ca433-b4e7-4185-b187-f88d0d116b9b

--- src/ObLib/Domain/Run.cs
+++ src/ObLib/Domain/Run.cs
@@ -58,6 +58,132 @@ namespace ObLib.Domain
             Trial.Save();
         }
 
+        public virtual void Export()
+        {
+            List<Behavior> allStateBehaviorsOnThisBehavioralType = new List<Behavior>();
+            List<Behavior> allInstantBehaviorsOnThisBehavioralType = new List<Behavior>();
+
+            foreach (Behavior behavior in Behavior.All())
+            {
+                if (behavior.BehavioralTestType == this.Trial.Session.BehavioralTest.BehavioralTestType)
+                {
+                    if (behavior.Type == Behavior.BehaviorType.State)
+                    {
+                        if (!allStateBehaviorsOnThisBehavioralType.Contains(behavior))
+                        {
+                            allStateBehaviorsOnThisBehavioralType.Add(behavior);
+                        }
+                        continue;
+                    }
+                    if (behavior.Type == Behavior.BehaviorType.Instant)
+                    {
+                        if (!allInstantBehaviorsOnThisBehavioralType.Contains(behavior))
+                        {
+                            allInstantBehaviorsOnThisBehavioralType.Add(behavior);
+                        }
+                        continue;
+                    }
+                }
+            }
+
+            Dictionary<Behavior, int> instantBehaviorCount = new Dictionary<Behavior, int>();
+            Dictionary<Behavior, long> stateBehaviorAverage = new Dictionary<Behavior, long>();
+
+            foreach (Behavior behavior in allInstantBehaviorsOnThisBehavioralType)
+            {
+                instantBehaviorCount.Add(behavior, 0);
+            }
+            foreach (Behavior behavior in allStateBehaviorsOnThisBehavioralType)
+            {
+                stateBehaviorAverage.Add(behavior, 0);
+            }
+
+            RunEvent lastStateRunEvent = null;
+            // TODO Order by timeTracked
+            foreach (RunEvent runEvent in RunEvents)
+            {
+                if (runEvent.TimeTracked == 0)
+                {
+                    lastStateRunEvent = runEvent;
+                    continue;
+                }
+
+                if (runEvent.Behavior.Type == Behavior.BehaviorType.Instant)
+                {
+                    instantBehaviorCount[runEvent.Behavior] += 1;
+                    continue;
+                }
+
+                if (lastStateRunEvent == null)
+                {
+                    Logger.logError(String.Format("no state behavior at time 0 found before this one {0}, {1}",
+                        this.Trial.Session.BehavioralTest.Project, this));
+                }
+
+                // State Behaviors tracked other than the first.
+                stateBehaviorAverage[lastStateRunEvent.Behavior] += runEvent.TimeTracked - lastStateRunEvent.TimeTracked;
+                lastStateRunEvent = runEvent;
+            }
+            // add the time left till the end of the run
+            stateBehaviorAverage[lastStateRunEvent.Behavior] += this.Trial.Duration * 1000 - lastStateRunEvent.TimeTracked;
+
+            // EXPORTING
+            List<string> headers = new List<string>();
+            List<string> data = new List<string>();
+
+            headers.Add("Project");
+            headers.Add("SubjectID");
+            headers.Add("SubjectGroup");
+            headers.Add("Strain");
+            headers.Add("Sex");
+            headers.Add("TrialName");
+            headers.Add("TrialDuration");
+            headers.Add("DateRun");
+            headers.Add("TimeRun");
+
+            data.Add(this.Trial.Session.BehavioralTest.Project.ToString());
+            data.Add(this.Subject.ToString());
+            string subjectGroup = "";
+            if (this.Subject.SubjectGroup != null)
+            {
+                subjectGroup = this.Subject.SubjectGroup.ToString();
+            }
+            data.Add(subjectGroup);
+            data.Add(this.Subject.Strain);
+            data.Add(this.Subject.Sex);
+            data.Add(this.Trial.ToString());
+            data.Add(this.Trial.Duration.ToString());
+            data.Add(this.TmCreated.ToString("dd/MM/yyyy"));
+            data.Add(this.TmCreated.ToString("HH:mm:ss"));
+
+            foreach (var behaviorAverage in stateBehaviorAverage)
+            {
+                headers.Add(behaviorAverage.Key.Name);
+                data.Add(behaviorAverage.Value.ToString());
+            }
+
+            foreach (var behaviorCount in instantBehaviorCount)
+            {
+                headers.Add(behaviorCount.Key.Name);
+                data.Add(behaviorCount.Value.ToString());
+            }
+
+
+            string headerRow = String.Join<String>("\t", headers);
+            string dataRow = String.Join<String>("\t", data);
+
+            string exportFilename = String.Format(@"{0}-{1}-{2}.csv",
+                this.Trial.Session.BehavioralTest.Project,
+                this.Trial,
+                this.Subject);
+
+            using (System.IO.StreamWriter file = new System.IO.StreamWriter(exportFilename))
+            {
+                file.WriteLine(headerRow);
+                file.WriteLine(dataRow);
+            }
+        }
+
         public override string ToString()
         {
             return String.Format("{0}, {1}", Trial, Subject);
