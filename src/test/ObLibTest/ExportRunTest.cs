fa8a77f6cf7cf9d0affc5be71f1d110dad1f42d7
dbaltas <dbaltas@672ca433-b4e7-4185-b187-f88d0d116b9b>
2012-09-09 21:34:50 +0000

    created unit test project for ObLib
    git-svn-id: https://codemax.unfuddle.com/svn/codemax_observador-svn@131 672ca433-b4e7-4185-b187-f88d0d116b9b

---
+++ src/test/ObLibTest/ExportRunTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ObLibTest
+{
+    public class ExportRunTest
+    {
+        [Test]
+        public void testNunitSetup()
+        {
+            Assert.AreEqual(5, 7 - 2);
+        }
+    }
+}
